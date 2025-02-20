4c25adba72b023c73054457b706787e42128c9bb
root <root@172-235-6-159.ip.linodeusercontent.com>
2025-02-20 12:08:24 +0000

    test

--- terraform/api_test.tf
+++ terraform/api_test.tf
@@ -7,23 +7,30 @@ terraform {
   }
 }
 
- 
-
 provider "catchpoint" {
-api_token="B27B80758D93018D781C80A63769D84F73765B4AB6889D24B8DAC396576A2F4A"
+  api_token = "B27B80758D93018D781C80A63769D84F73765B4AB6889D24B8DAC396576A2F4A"
 }
 
- 
+resource "api_test" "test67890" {
+  test_name        = "APItest_TF_CICD"
+  provider        = catchpoint
+  division_id     = 8098
+  product_id      = 24405
+  test_script_type = "selenium"
+  start_time      = "2025-02-18T04:59:00Z"
+  end_time        = "2025-12-30T04:59:00Z"
 
- 
+  test_script = <<EOT
+// Step 1: Check Products API
+open("http://172.235.6.159:3000/products")
 
-resource "api_test" "test1234555" {
-  test_name  = "APItest_TF_12345"
-  provider=catchpoint
-  division_id=8098
-  product_id=24405
-  test_script="// Step - 1\r\nopen(\"\\\"http://172.235.6.159:3000/health)\")"
-  test_script_type="selenium"
-  start_time = "2025-02-18T04:59:00Z"
-  end_time="2025-12-30T04:59:00Z"
+// Step 2: Search for 'Laptop'
+open("http://172.235.6.159:3000/search?q=laptop")
+
+// Step 3: Open cart
+open("http://172.235.6.159:3000/cart")
+
+EOT
 }
+
+
--- terraform/testing/api_test.tf
+++ terraform/testing/api_test.tf
@@ -7,42 +7,29 @@ terraform {
   }
 }
 
- 
-
 provider "catchpoint" {
-api_token="B27B80758D93018D781C80A63769D84F73765B4AB6889D24B8DAC396576A2F4A"
+  api_token = "B27B80758D93018D781C80A63769D84F73765B4AB6889D24B8DAC396576A2F4A"
 }
 
- 
+resource "api_test" "test1123234555" {
+  test_name        = "APItest_TF_1122345"
+  provider        = catchpoint
+  division_id     = 8098
+  product_id      = 24405
+  test_script_type = "selenium"
+  start_time      = "2025-02-18T04:59:00Z"
+  end_time        = "2025-12-30T04:59:00Z"
 
- 
-resource "web_test" "test" {
-    provider=catchpoint
-    division_id=8098
-    product_id=25331
-    test_name="catchpointsheldonTf"
-    test_url="https://www.catchpoint.com"
-    enable_test_data_webhook=true
-    end_time="2025-02-20T04:59:00Z"
-}
-  request_settings {
-        authentication {
-          authentication_type = "basic"
-          password_ids = [2332]
-        }
-        token_ids = [1096]
-        http_request_headers {
-          user_agent {
-            value = "vikash"
-          }
-        }
-      }
+  test_script = <<EOT
+// Step 1: Check Products API
+open("http://172.235.6.159:3000/products")
 
-      schedule_settings{
-      frequency="6 hours"
-      node_distribution ="random"
-      no_of_subset_nodes = 5
-      node_ids =[6388]
-      node_group_ids =[9922,9848]
-    }
+// Step 2: Search for 'Laptop'
+open("http://172.235.6.159:3000/search?q=laptop")
+
+// Step 3: Open cart
+open("http://172.235.6.159:3000/cart")
+
+EOT
 }
+
